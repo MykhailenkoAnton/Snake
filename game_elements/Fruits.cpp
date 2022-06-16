cf38e59eb41f2b3b7567ecf97d055297b8525663
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-06-17 01:28:47 +0300

    const

--- game_elements/Fruits.cpp
+++ game_elements/Fruits.cpp
@@ -1,11 +1,13 @@
 #include "Fruits.h"
 #include <cstdlib>
 #include <ctime>
+const int MAX_ROW = 13;
+const int MAX_COL = 18;
 Fruits::Fruits()
 {
     srand(time(0));
-    frui.x = rand() % 13;
-    frui.y = rand() % 18;
+    frui.x = rand() % MAX_ROW;
+    frui.y = rand() % MAX_COL;
 }
 
 fruit_coord Fruits::get_coord_fruit()
@@ -16,6 +18,6 @@ fruit_coord Fruits::get_coord_fruit()
 void Fruits::set_new_coord_for_fruit()
 {
     srand(time(0));
-    frui.x = rand() % 13;
-    frui.y = rand() % 18;
+    frui.x = rand() % MAX_ROW;
+    frui.y = rand() % MAX_COL;
 }
